bc8ec0be5fbdd13c9a532e41bed2b3822c7945a3
BrianDemorcy <501526@containerstore.com>
2022-05-27 14:39:58 -0400

    test package

---
+++ src/lib/UiComponents.js
@@ -0,0 +1,9 @@
+import styles from './UiComponents.module.scss';
+export function TestComponent(props) {
+  return (
+    <div className={styles['container']}>
+      <h1>Tula test package</h1>
+    </div>
+  );
+}
+export default TestComponent;
